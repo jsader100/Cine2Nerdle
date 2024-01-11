c49ac2dfd93d2c32a63efa39c902cf8d0d664879
John Sader <129446429+jsader100@users.noreply.github.com>
2024-01-11 13:22:30 -0600

    Initial commit

---
+++ game_bot.py
@@ -0,0 +1,111 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from tmdb_scrape import *
+from datetime import datetime
+import random
+
+import time
+
+def play_game():
+
+    service = Service(executable_path="/Users/johnsader/PycharmProjects/Cine2Nerdle/chromedriver")
+    driver = webdriver.Chrome(service=service)
+
+    link = input("Paste Gamelink: ")
+
+    driver.get(link)
+
+    time.sleep(2)
+
+    link = driver.find_element(By.CLASS_NAME, "battle-home-button")
+    link.click()
+
+    time.sleep(3)
+
+    add_bans(driver)
+
+    time.sleep(20)
+
+    main_game(driver)
+
+    driver.quit()
+
+def add_bans(driver):
+
+    time.sleep(3)
+
+
+    input_element = driver.find_elements(By.CLASS_NAME, "battle-input")
+    input_element[0].send_keys("Robert De Niro" + Keys.ENTER)
+    input_element[1].send_keys("Martin Scorsese" + Keys.ENTER)
+    input_element[2].send_keys("Leonardo DiCaprio" + Keys.ENTER)
+
+    time.sleep(3)
+
+    link = driver.find_element(By.CLASS_NAME, "battle-choose-bans-button")
+
+    link.click()
+
+
+def main_game(driver):
+
+    game_end = False
+
+
+
+    while not game_end:
+
+        WebDriverWait(driver, 60).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "battle-input"))
+        )
+
+
+        input_element = driver.find_element(By.CLASS_NAME, "battle-input")
+
+        time.sleep(2)
+
+        last_played_movie = driver.find_element(By.CLASS_NAME, "battle-board-movie")
+
+
+        print(last_played_movie.text)
+        movie_title = last_played_movie.text.split()
+        print(movie_title)
+        movie_year = str(movie_title[-1][1:-1])
+        movie_title = " ".join(movie_title[2:-1])
+
+        move_id = get_movie_id(movie_title, movie_year)
+        movie_list = get_connected_movies(move_id)
+
+        input_element.send_keys(movie_list[random.randint(0, 3)][random.randint(0,4)])
+        link = driver.find_element(By.CLASS_NAME, "fa-sharp")
+
+        link.click()
+
+        time.sleep(1)
+
+
+
+play_game()
+
+
+
+'''WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "gLFyf"))
+)
+
+input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+input_element.clear()
+input_element.send_keys("tech with tim" + Keys.ENTER)
+
+link = driver.find_element(By.PARTIAL_LINK_TEXT, "Tech With Tim")
+
+link.click()
+
+time.sleep(5)
+
+driver.quit()
+'''
---
+++ tmdb_scrape.py
@@ -0,0 +1,120 @@
+import requests
+
+file_path = 'Authorization_Token.gitignore'
+with open(file_path, 'r') as file:
+    token = file.read()
+
+
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer " + token
+}
+
+
+
+
+
+def prompt_movie():
+    done = False
+
+    while not done:
+
+        movie = input("\nMovie: ")
+        year = input("Year: ")
+
+        id = get_movie_id(movie, year)
+        if id != -1:
+            print_cast(id)
+        else:
+            print("Movie not found :(")
+
+        user_input = input("\nDone (y/n): ")
+        done = user_input == "y"
+
+def get_movie_id(movie, year):
+    page = 1
+
+    while page < 501:
+
+        url = f'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page={page}&primary_release_year={year}&sort_by=popularity.desc'
+
+        response = requests.get(url, headers=headers)
+        results = response.json()
+
+        for j in range(20):
+            if results['results'][j]['title'] == movie:
+                return results['results'][j]['id']
+
+        page += 1
+
+    return -1
+
+
+def print_cast(id):
+    url = f"https://api.themoviedb.org/3/movie/{id}/credits?language=en-US"
+
+    response = requests.get(url, headers=headers)
+
+    results = response.json()
+
+    for i in range(4):
+        print("\n" + results['cast'][i]['name'] + ":")
+        print_cast_movies(results['cast'][i]['id'])
+
+def print_cast_movies(pid):
+    url = f"https://api.themoviedb.org/3/person/{pid}/combined_credits?language=en-US"
+
+    response = requests.get(url, headers=headers)
+    results = response.json()
+
+    for i in range(5):
+        print_movie(results['cast'][i]['id'])
+
+def print_movie(id):
+    url = f"https://api.themoviedb.org/3/movie/{id}?language=en-US"
+
+    response = requests.get(url, headers=headers)
+    results = response.json()
+
+    print(results['title'])
+
+
+def get_cast_movies(pid):
+
+    return_list = []
+    url = f"https://api.themoviedb.org/3/person/{pid}/combined_credits?language=en-US"
+
+    response = requests.get(url, headers=headers)
+    results = response.json()
+
+    for i in range(5):
+        return_list.append(get_movie(results['cast'][i]['id']))
+
+    return return_list
+def get_connected_movies(id):
+
+    return_list = []
+
+    url = f"https://api.themoviedb.org/3/movie/{id}/credits?language=en-US"
+
+    response = requests.get(url, headers=headers)
+
+    results = response.json()
+
+    for i in range(4):
+        return_list.append(get_cast_movies(results['cast'][i]['id']))
+
+
+    return return_list
+
+def get_movie(id):
+    url = f"https://api.themoviedb.org/3/movie/{id}?language=en-US"
+
+    response = requests.get(url, headers=headers)
+    results = response.json()
+
+    return results['title'] + " (" + results['release_date'][:4] + ")"
+
+#print(get_connected_movies(238))
+
+print(get_movie_id("The Godfather", 1972))
